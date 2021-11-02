8c830261a6f2df67dca33fbf6bdf4952dbcc64c1
AllexK87 <allex.k87@ya.ru>
2021-11-03 02:33:16 +0300

    works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Burger menu
 const burger = document.querySelector('.header__icon');
 
 burger.addEventListener('click', function () {
@@ -6,4 +7,25 @@ burger.addEventListener('click', function () {
     menu.classList.toggle('menu-open');
     const bars = document.querySelector('.ic-bars');
     bars.classList.toggle('ic-plus');
+})
+
+// Slaider works
+const swiper = new Swiper('.works__slieder', {
+    autoplay: {
+        delay: 5000,
+      },
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2.5,
+        },
+        1024: {
+          slidesPerView: 2.5,
+        },
+      },
 })
